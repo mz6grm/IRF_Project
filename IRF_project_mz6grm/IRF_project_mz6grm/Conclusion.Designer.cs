92e7a64f603a245b6ecbf1062352b2bf32d827af
Csaba Kaló <kalo.csk@gmail.com>
2020-12-11 02:28:04 +0100

    Konklúziós felület készen áll a befejezésre.

--- IRF_project_mz6grm/IRF_project_mz6grm/Conclusion.Designer.cs
+++ IRF_project_mz6grm/IRF_project_mz6grm/Conclusion.Designer.cs
@@ -28,12 +28,76 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(23, 62);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(369, 26);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Az adatokból megállapítható, hogy ...";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label2.Location = new System.Drawing.Point(401, 131);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(51, 20);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "label2";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label3.Location = new System.Drawing.Point(401, 189);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 20);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label4.Location = new System.Drawing.Point(401, 246);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(51, 20);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "label4";
+			// 
+			// Conclusion
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Conclusion";
 			this.Text = "Conclusion";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
 	}
 }
