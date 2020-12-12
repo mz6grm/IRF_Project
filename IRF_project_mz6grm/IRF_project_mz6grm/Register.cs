40240fa433933f6e63da18ab73cc0fef95dec96f
Csaba Kaló <kalo.csk@gmail.com>
2020-12-12 02:48:13 +0100

    Regisztráció lekódolása kezdetlegesen kész.

--- IRF_project_mz6grm/IRF_project_mz6grm/Register.cs
+++ IRF_project_mz6grm/IRF_project_mz6grm/Register.cs
@@ -12,6 +12,7 @@ namespace IRF_project_mz6grm
 {
 	public partial class Register : Form
 	{
+        DatabaseEntities context = new DatabaseEntities();
 		public Register()
 		{
 			InitializeComponent();
@@ -19,7 +20,25 @@ namespace IRF_project_mz6grm
 
 		private void button1_Click(object sender, EventArgs e)
 		{
+            LOGIN u = new LOGIN();
+            u.Username = textBox1.Text;
+            u.Password = textBox2.Text;
+            u.Email_Address = textBox3.Text;
+            u.Phone_Number = Convert.ToInt32(textBox4.Text);            
 
-		}
+            var i = (from x in context.LOGINs select x.Username).ToList();
+            if (!i.Contains(u.Username))
+            {
+                context.LOGINs.Add(u);
+                context.SaveChanges();
+                MessageBox.Show("Sikeres regisztráció!");
+
+                this.Hide();
+                Form1 login_window = new Form1();
+                login_window.Show();
+            }
+            else
+                MessageBox.Show("A felhasználónév már foglalt!");
+        }
 	}
 }
